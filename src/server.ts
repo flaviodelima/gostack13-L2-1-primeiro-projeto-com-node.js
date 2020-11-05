9f09d39799ac17a937ba7766b7da32ce09b6058e
Flavio de Lima <flaviolimpio@gmail.com>
2020-11-05 00:18:06 -0300

    01.03 - Criada a rota users

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import express, { request, response } from 'express';
+import express from 'express';
+import routes from './routes/index';
 
 const app = express();
 
+app.use(express.json());
+app.use(routes);
+
 app.get('/', (request, response) => {
-    return response.json({message: 'Hello World'});
-})
+  return response.json({ message: 'Hello World' });
+});
 
 const port = 3333;
-app.listen(port, ()=>{
-    console.log(`🚀 Server started on port ${port}` )
-})
+app.listen(port, () => {
+  console.log(`🚀 Server started on port ${port}`);
+});
