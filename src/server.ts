c3863aafbc558950efa76972b10484e77756c384
Flavio de Lima <flaviolimpio@gmail.com>
2020-11-06 00:00:49 -0300

    02.03 - Cadastro de Agendamentos

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
-
 const port = 3333;
 app.listen(port, () => {
   console.log(`🚀 Server started on port ${port}`);
