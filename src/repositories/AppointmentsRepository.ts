c8ea13a1ce4fb4a5bf1d3feb1591b59a8a84edd4
Flavio de Lima <flaviolimpio@gmail.com>
2020-11-06 01:20:18 -0300

    02.06 - Criando repositórios

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,28 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const hasAppointmentInDate = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return hasAppointmentInDate || null;
+  }
+}
+
+export default AppointmentsRepository;
