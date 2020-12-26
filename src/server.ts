246400d579f0efc996fd739fc0b3209c81068412
Flavio de Lima <flaviolimpio@gmail.com>
2020-12-26 15:09:40 -0300

    L2-2 04.03 - Servindo arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes/index';
+import uploadConfig from './config/upload';
 
 import './database';
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/files', express.static(uploadConfig.directory));
 
 const port = 3333;
 app.listen(port, () => {
