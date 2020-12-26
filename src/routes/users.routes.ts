8f7bc463d52005c4e3679c32ef44ad4c3d096db1
Flavio de Lima <flaviolimpio@gmail.com>
2020-12-26 20:05:01 -0300

    L2-2 05.02 - Lidando com erros

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,24 +11,20 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserservice();
+  const createUser = new CreateUserservice();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    // that's the classes solution, but it brakes contract with the interface for user
-    // delete user.password;
+  // that's the classes solution, but it brakes contract with the interface for user
+  // delete user.password;
 
-    return response.json({ name: user.name, email: user.email });
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json({ name: user.name, email: user.email });
 });
 
 usersRouter.patch(
@@ -36,17 +32,13 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = await new UpdateUserAvatarService();
-      await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarFilename: request.file.filename,
-      });
+    const updateUserAvatar = await new UpdateUserAvatarService();
+    await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
 
-      return response.json({ ok: true });
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json({ ok: true });
   },
 );
 
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,35 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors'; // needed for the global exception handler
+
 import routes from './routes/index';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 const port = 3333;
 app.listen(port, () => {
