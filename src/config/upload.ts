c0406a6bee55babd67c6a7fd62a3be499f4b5588
Flavio de Lima <flaviolimpio@gmail.com>
2020-12-26 15:08:40 -0300

    L2-2 04.02 - Atualizando avatar

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,12 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
+
 export default {
+  directory: tmpFolder,
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'temp'),
+    destination: tmpFolder,
 
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
